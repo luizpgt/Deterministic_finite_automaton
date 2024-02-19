014d3053b80dd8de4d69cd81e9c7a0ec8545e26d
luizpgt <luiz.paulus51@gmail.com>
2024-02-19 16:23:51 -0300

    🎉 init: first local commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+set -xe
+g++ -Wall -Wextra -o run.o main.cpp
+./run.o
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int {
+    std::cout << "projeto bomba atômica!" << std::endl;
+    return 0;
+}
